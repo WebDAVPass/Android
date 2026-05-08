73394688a8a4a75a27739fbb38fb569d08af29a8
xzynine <xzyht-nine@outlook.com>
2026-05-08 23:03:13 +0800

    build: 更新构建配置以支持Parcelize插件
    - 在主构建文件中添加kotlin-parcelize插件
    - 将database模块的插件应用方式更新为新格式
    - 修改icon-pack模块的依赖声明从implementation改为api

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "2.2.21" apply false
 }
